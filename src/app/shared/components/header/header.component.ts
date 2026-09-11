6d228c2088a494bf529ff4f8879e34b7a08ea9e4
Stiven Gomez <stivenalexander7u7@gmail.com>
2026-09-11 01:45:36 -0500

    ADD - Se ajusta responsive.

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-// import { StorageService } from '../../../core/services/storage.service';
-// import { ItemsMenuModel } from '../../../core/models/items-menu.model';
-// import { DataAppService } from '../../../core/services/data-app.service';
 
 @Component({
   selector: 'app-header',
@@ -10,31 +7,5 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
-  // itemsMenu: ItemsMenuModel[] = [];
-
-  namePage = "";
-
-  // constructor(
-  //   private storageService: StorageService,
-  //   private dataAppService: DataAppService
-  // ) {}
-
-  // ngOnInit(): void {
-  //   this.dataAppService.getIsNamePage().subscribe((page) => {
-  //     this.namePage = page;
-  //   });
-
-
-  //   let path = document.location.pathname;
-  //   let itemsMenu = this.storageService.readStorage('itemsMenu') as ItemsMenuModel[] | null;
-
-  //   if (itemsMenu) {
-  //     let menuActual = itemsMenu.find((menu) => menu.url == path);
-
-  //     if (menuActual) {
-  //       this.dataAppService.setIsNamePage(menuActual.name)
-  //     }
-  //   }
-  // }
-
+  namePage = "TalycapGlobalFrontend";
 }
